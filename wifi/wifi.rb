9425a00e1cc51bcecf1bcb2c8c2021f211a7eb20
Christoph Kappel <christoph@unexist.dev>
2010-03-14 21:40:20 +0100

    Updated wifi sublet

--- wifi/wifi.rb
+++ wifi/wifi.rb
@@ -1,25 +1,37 @@
 # Wifi sublet file
 # Created with sur-0.1
+require "socket"
+
+# Copied from wireless.h
+SIOCGIWESSID      = 0x8B1B  
+IW_ESSID_MAX_SIZE = 32
+
 configure :wifi do |s|
-  self.interval = 240
-  s.icon        = Subtle::Icon.new("wifi_01.xbm")
-
-  # Precompile regex
-  s.re_essid     = Regexp.new('.*ESSID:"?(.*?)"?\s*\n')
-  s.re_quality   = Regexp.new('.*Quality:?=? ?(\d+)\s*/?\s*(\d*)')
-
-  # Select wifi device
-  s.device       = "wlan0"
+  s.interval = 240
+  s.icon     = Subtle::Icon.new("wifi_01.xbm")
+  s.device   = "wlan0"
 end
 
 on :run do |s|
   begin
-    # Fetch data from iwconfig
-    iwconfig         = `iwconfig #{s.device}`
-    match, essid     = iwconfig.match(s.re_essid).to_a
-    match, low, high = iwconfig.match(s.re_quality).to_a
+    # Get data
+    wireless = IO.readlines("/proc/net/wireless", "r")
 
-    s.data = "%s%s (%d/%d)" % [ s.icon, essid, low, high ]
+    device, link, level, noise = wireless.join.scan(/(\w*):\s*\d*\s*([0-9-]+).\s+([0-9-]+).\s+([0-9-]+)/).flatten
+
+    # Get essid
+    sock = Socket.new(Socket::AF_INET, Socket::SOCK_DGRAM, 0)
+
+    templ   = "a16pI2"
+    iwessid = [ device, " " * IW_ESSID_MAX_SIZE, IW_ESSID_MAX_SIZE, 1 ].pack(templ)
+
+    sock.ioctl(SIOCGIWESSID, iwessid)
+
+    interface, essid, len, flags = iwessid.unpack(templ)
+
+    max = 100
+
+    s.data = "%s%s (%d/%d)" % [ s.icon, essid.strip, link, max ]
   rescue => err # Sanitize to prevent unloading
     s.data = "subtle"
     p err
--- wifi/wifi.spec
+++ wifi/wifi.spec
@@ -3,11 +3,11 @@
 Sur::Specification.new do |s|
   s.name        = "Wifi"
   s.authors     = [ "Christoph Kappel" ]
-  s.date        = "Sat Sep 05 11:00 CET 2009"
+  s.date        = "Sun Mar 14 21:39 CET 2010"
   s.contact     = "unexist@dorfelite.net"
   s.description = "Show the status of a wifi device"
-  s.version     = "0.2"
-  s.tags        = ["Shell", "Icon" ]
+  s.version     = "0.3"
+  s.tags        = ["Socket", "Icon" ]
   s.files       = [ "wifi.rb" ]
   s.icons       = [ "icons/wifi_01.xbm" ]
 end
