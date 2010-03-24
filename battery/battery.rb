b4cbb7c71a5add94c0261d9ef454f7133de90919
Christoph Kappel <christoph@unexist.dev>
2010-03-24 21:42:31 +0100

    Added support for energy_* files

--- battery/battery.rb
+++ battery/battery.rb
@@ -1,42 +1,66 @@
 # Battery sublet file
 # Created with sur-0.1
 configure :battery do |s|
-  s.interval   = 60
-  s.full       = 0
-  s.path       = ""
-  s.icon_ac    = Subtlext::Icon.new("ac.xbm")
-  s.icon_full  = Subtlext::Icon.new("bat_full_02.xbm")
-  s.icon_low   = Subtlext::Icon.new("bat_low_02.xbm")
-  s.icon_empty = Subtlext::Icon.new("bat_empty_02.xbm")
+  s.interval = 60
+  s.full     = 0
 
-  # Get battery slot and capacity
+  # Path
+  s.now      = ""
+  s.status   = ""
+
+  # Icons
+  s.icons = {
+    :ac    => Subtlext::Icon.new("ac.xbm"),
+    :full  => Subtlext::Icon.new("bat_full_02.xbm"),
+    :low   => Subtlext::Icon.new("bat_low_02.xbm"),
+    :empty => Subtlext::Icon.new("bat_empty_02.xbm")
+  }
+
+  # Find battery slot and capacity
   begin
-    s.path = Dir["/sys/class/power_supply/B*"].first
-    s.full = IO.readlines(s.path + "/charge_full").first.to_i
-  rescue 
-    raise "Could't find battery"
+    path = Dir["/sys/class/power_supply/B*"].first
+    now  = ""
+    full = ""
+
+    if(File.exist?(File.join(path, "charge_full")))
+      full = "charge_full"
+      now  = "charge_now"
+    elsif(File.exist?(File.join(path, "energy_full_design")))
+      full = "energy_full_design"
+      now  = "energy_now"
+    end
+
+    # Assemble paths
+    s.now    = File.join(path, now)
+    s.status = File.join(path, "status")
+
+    # Get full capacity
+    s.full = IO.readlines(File.join(path, full)).first.to_i
+  rescue => err
+    puts err, err.backtrace
+    raise "Could't find any battery"
   end
 end
 
 on :run do |s|
   begin
-    now     = IO.readlines(s.path + "/charge_now").first.to_i
-    state   = IO.readlines(s.path + "/status").first.chop
+    now     = IO.readlines(s.now).first.to_i
+    state   = IO.readlines(s.status).first.chop
     percent = (now * 100 / s.full).to_i
 
     # Select icon
     icon = case state
-      when "Charging"  then s.icon_ac
+      when "Charging"  then :ac
       when "Discharging"
         case percent
-          when 67..100 then s.icon_full
-          when 34..66  then s.icon_low
-          when 0..33   then s.icon_empty
+          when 67..100 then :full
+          when 34..66  then :low
+          when 0..33   then :empty
         end
-      when "Full"      then s.icon_ac
+      when "Full"      then :ac
     end
 
-    s.data = "%d%%%s" % [ percent, icon ]
+    s.data = "%d%%%s" % [ percent, s.icons[icon] ]
   rescue => err # Sanitize to prevent unloading
     s.data = "subtle"
     p err
