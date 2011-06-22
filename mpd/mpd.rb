e4220f66d943dc3bd60607cc323124ff4bb61977
Christoph Kappel <christoph@unexist.dev>
2011-06-22 17:52:27 +0200

    Fixed toggle_state when socket is closed and def_action isn't toggle

--- mpd/mpd.rb
+++ mpd/mpd.rb
@@ -1,6 +1,7 @@
 # Mpd sublet file
 # Created with sur-0.1
 require "socket"
+require "subtle/subtlext"
 
 # Class Pointer {{{
 class Pointer
@@ -57,7 +58,12 @@ class Mpd
 
   def connect
     begin
-      @socket = TCPSocket.new(@host, @port)
+      # Check socket type
+      if(File.socket?(@host))
+        @socket = UNIXSocket.new(@host)
+      else
+        @socket = TCPSocket.new(@host, @port)
+      end
 
       # Handle SIGPIPE
       trap "PIPE" do
@@ -102,7 +108,6 @@ class Mpd
   ##
 
   def action(command)
-		p "command=#{command}"
     noidle
     safe_write(command)
   end # }}}
@@ -274,7 +279,7 @@ class Mpd
 
   def parse_current
     unless(@socket.nil?)
-      @current_song = get_reply("currentsong") 
+      @current_song = get_reply("currentsong")
     else
       @current_song = {}
     end
@@ -296,37 +301,68 @@ configure :mpd do |s| # {{{
   }
 
   # Options
-  s.def_action     = s.config[:def_action]
-  s.wheel_up       = s.config[:wheel_up]
-  s.wheel_down     = s.config[:wheel_down]
-  s.format_string  = s.config[:format_string] || "%note%%artist% - %title%"
+  s.def_action       = s.config[:def_action]
+  s.wheel_up         = s.config[:wheel_up]
+  s.wheel_down       = s.config[:wheel_down]
+  s.format_string    = s.config[:format_string]    || "%note%%artist% - %title%"
+  s.not_running_text = s.config[:not_running_text] || "-- not running --"
+  s.stop_text        = s.config[:stop_text]        || "-- stopped --"
+  s.pause_text       = s.config[:pause_text]       || "-- paused --"
+  s.blank_text       = s.config[:blank_text]       || "n/a"
+  s.show_icons       = s.config[:show_icons].nil? ? true : s.config[:show_icons]
+  s.show_pause       = s.config[:show_pause].nil? ? true : s.config[:show_pause]
+
+  # Colors
+  if(s.config[:show_colors])
+    s.colors = {
+      :note  => "#ffffff", :artist => "#757575",
+      :album => "#757575", :title  => "#757575",
+      :track => "#757575", :id     => "#757575",
+      :pause => "#FECF35", :stop   => "#757575",
+    }
+
+    s.colors.each do |k, v|
+      s.colors[k] = Subtlext::Color.new(s.config["#{k}_color".to_sym] || v)
+    end
+  else
+    s.colors = {}
+  end
 
   # Sanitize actions
-  valid = [ "play", "pause 0", "pause 1", "stop", "previous", "next", "stop" ]
+  valid = [ "play", "pause 0", "pause 1", "toggle", "stop", "previous", "next" ]
 
-  s.def_action = "next"     unless(valid.include?(s.def_action))
+  s.def_action = "toggle"   unless(valid.include?(s.def_action))
   s.wheel_up   = "next"     unless(valid.include?(s.wheel_up))
   s.wheel_down = "previous" unless(valid.include?(s.wheel_down))
 
   # Parse format string once
-  fields = [ "%note%", "%artist%", "%album%", "%title%", "%track%", "%id%" ]
+  fields = [ "note", "artist", "album", "title", "track", "id" ]
 
   s.format_values = {}
 
-  s.format_string.gsub!(/%[^%]+%/) do |f|
-    if(fields.include?(f))
-      name = f.delete("%")
+  s.format_string.gsub!(/%([^%]+)%/) do
+    field = $1
+    out   = ""
 
-      if("%note%" == f)
-        format_values[name] = self.icons[:note]
+    # Add colors before fields
+    if(s.colors.has_key?(field.to_sym))
+      s.format_values["#{field}_color"] = Pointer.new(s.colors[field.to_sym])
+
+      out << "%s"
+    end
+
+    # Add fields
+    if(fields.include?(field))
+      if("note" == field)
+        s.format_values[field] = self.icons[:note]
       else
-        format_values[name] = Pointer.new
+        s.format_values[field] = Pointer.new
       end
 
-      "%s"
-    else
-      ""
+      out << "%s"
     end
+
+    out
   end
 
   # Create mpd object
@@ -341,78 +377,134 @@ configure :mpd do |s| # {{{
 end # }}}
 
 helper do |s| # {{{
-  def update_status # {{{
-    mesg  = "mpd not running"
-    modes = ""
-    icon  = :play
-
-    unless(self.mpd.socket.nil?)
-      if(:play == self.mpd.state or :pause == self.mpd.state)
-        # Select icon
-        icon = case self.mpd.state
-          when :play  then :pause
-          when :pause then :play
-        end
-
-        # Sanity?
-        self.format_values.each do |k, v|
-          if(self.mpd.current_song.include?(k))
-            self.format_values[k].value = self.mpd.current_song[k] || "n/a"
-          end
-        end
-
-        # Modes
-        modes << self.icons[:repeat]   if(self.mpd.repeat)
-        modes << self.icons[:random]   if(self.mpd.random)
-        modes << self.icons[:database] if(self.mpd.database)
-        modes = " %s" % [ modes ] unless(modes.empty?)
-
-        # Assemble format
-        mesg = self.format_string % self.format_values.values
-      elsif(:stop == self.mpd.state)
-        mesg = "mpd stopped"
-        icon = :play
+  def current_song # {{{
+    # Replace empty values by blank text
+    self.format_values.each do |k, v|
+      if(self.mpd.current_song.has_key?(k))
+        v.value = (self.mpd.current_song[k].nil? or
+          self.mpd.current_song[k].empty? ? self.blank_text :
+            self.mpd.current_song[k])
+      # Set field to blank text
+      elsif(v.is_a?(Pointer) and !k.end_with?("_color"))
+        v.value = self.blank_text
       end
     end
 
-    self.data = "%s%s%s%s%s %s" % [
-      self.icons[icon], self.icons[:stop],
-      self.icons[:prev], self.icons[:next],
-      modes, mesg
-    ]
+    # Assemble format
+    self.format_string % self.format_values.values
+  end # }}}
 
+  def update_status # {{{
+    mesg  = self.not_running_text
+    icon  = :play
+    modes = ""
+
+    unless(self.mpd.socket.nil?)
+      # Modes
+      modes << self.icons[:repeat]   if(self.mpd.repeat)
+      modes << self.icons[:random]   if(self.mpd.random)
+      modes << self.icons[:database] if(self.mpd.database)
+      modes = " %s" % [ modes ] unless(modes.empty?)
+
+      # Assemble data based on state
+      case(self.mpd.state)
+        when :play
+          mesg = current_song
+          icon = :pause
+        when :pause
+          if(self.show_pause)
+            mesg = "%s%s" % [ self.colors[:pause] || "", self.pause_text ]
+          else
+            mesg = current_song
+          end
+
+          icon = :play
+        when :stop
+          mesg = "%s%s" % [ self.colors[:stop] || "", self.stop_text ]
+          icon = :play
+      end
+    end
+
+    # Add some icons
+    if(self.show_icons)
+      mesg = "%s%s%s%s%s %s" % [
+        self.icons[icon], self.icons[:stop],
+        self.icons[:prev], self.icons[:next],
+        modes, mesg
+      ]
+    end
+
+    self.data = mesg
+  end # }}}
+
+  def toggle_state(force = false) # {{{
+    if(force or "toggle" == self.def_action)
+      case self.mpd.state
+        when :stop  then "play"
+        when :pause then "pause 0"
+        when :play  then "pause 1"
+      end
+    else
+      self.def_action
+    end
+  end # }}}
+
+  def send_action(action) # {{{
+    # Reconnect if necessary
+    if(self.mpd.socket.nil?)
+      watch(self.mpd.socket) if(self.mpd.connect)
+      update_status
+    else
+      self.mpd.action(action)
+    end
   end # }}}
 end # }}}
 
 on :mouse_down do |s, x, y, b| # {{{
-  if(s.mpd.socket.nil?)
-    watch(s.mpd.socket) if(s.mpd.connect)
-    update_status
+  # Handle clicks based on x coord and state
+  if(s.show_icons)
+    action = case b
+      when 1
+        case x
+          when  0..15 then toggle_state(true)
+          when 16..31 then "stop"
+          when 32..47 then "previous"
+          when 48..63 then "next"
+          else toggle_state
+        end
+      when 4 then s.wheel_up
+      when 5 then s.wheel_down
+    end
   else
-    # Send to socket
-    s.mpd.action(
-      case b
-        when 1
-          case x
-            when 2..17
-              case s.mpd.state
-                when :stop  then "play"
-                when :pause then "pause 0"
-                when :play  then "pause 1"
-              end
-            when 18..33 then "stop"
-            when 34..49 then "previous"
-            when 50..65 then "next"
-            else s.def_action
-          end
-        when 4 then s.wheel_up
-        when 5 then s.wheel_down
-      end
-    )
+    action = case b
+      when 1 then toggle_state
+      when 4 then s.wheel_up
+      when 5 then s.wheel_down
+    end
   end
+
+  # Send to socket
+  send_action(action)
 end # }}}
 
 on :watch do |s| # {{{
   unwatch unless(s.mpd.update)
   update_status
+end # }}}
+
+# Grabs
+
+grab :MpdToggle do |s| # {{{
+  send_action(toggle_state)
+end # }}}
+
+# Other grabs {{{
+{
+  :MpdPlay => "play",  :MpdPause    => "pause 1",
+  :MpdStop => "stop",  :MpdPrevious => "previous",
+  :MpdNext => "next"
+}.each do |k, v|
+  grab k do |s|
+    send_action(v)
+  end
 end # }}}
